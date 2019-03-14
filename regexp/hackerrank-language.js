bd134fca9b4d544a923d662148f1625974f4c02c
Khranovskiy <xronosd@gmail.com>
2019-03-15 01:10:18 +0300

    hackerrank-language

---
+++ regexp/hackerrank-language.js
@@ -0,0 +1,22 @@
+function processData(input) {
+    const languages = 'C:CPP:JAVA:PYTHON:PERL:PHP:RUBY:CSHARP:\
+    HASKELL:CLOJURE:BASH:SCALA:ERLANG:CLISP:LUA:BRAINFUCK:\
+    JAVASCRIPT:GO:D:OCAML:R:PASCAL:SBCL:DART:GROOVY:OBJECTIVEC'
+    .split(':')
+    
+    const re = new RegExp(`^\\d{5} (${languages.join('|')})\$`, 'i')
+    input.trim().split('\n').slice(1)
+    .map(line => re.test(line) ? 'VALID': 'INVALID')
+    .forEach(status => console.log(status))
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
