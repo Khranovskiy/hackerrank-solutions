ce79c0be1005f96f4f7c552bbcc9ff4b4b06971b
Khranovskiy <xronosd@gmail.com>
2019-03-14 22:26:54 +0300

    solve hackerrank-tweets

---
+++ regexp/hackerrank-tweets.js
@@ -0,0 +1,34 @@
+
+/* 
+Practice > Regex > Applications > 
+
+    
+https://www.hackerrank.com/challenges/hackerrank-tweets/problem
+# Input Format
+# Output Format
+# Sample input
+# Sample Output
+*/
+
+const re = /hackerrank/i;
+
+function processData(input) {
+    const data = input.split('\n')
+    const n = parseInt(data[0])
+    const lines = data.slice(1);
+    const tweetCount = lines
+      .filter( el => re.test(el))
+      .length;
+      console.log(tweetCount)
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
